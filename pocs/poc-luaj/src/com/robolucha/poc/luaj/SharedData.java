cf36f9a090a57f130a0a1d8b04f62c0614dc5bcd
Hamilton Lima <hamilton.lima@gmail.com>
2017-09-12 21:15:16 -0400

    Implements Update java object from LUA code

---
+++ pocs/poc-luaj/src/com/robolucha/poc/luaj/SharedData.java
@@ -0,0 +1,30 @@
+package com.robolucha.poc.luaj;
+
+public class SharedData {
+
+	private float floatValue;
+	private String stringValue;
+	private int intValue;
+	
+	
+	public float getFloatValue() {
+		return floatValue;
+	}
+	public void setFloatValue(float floatValue) {
+		this.floatValue = floatValue;
+	}
+	public String getStringValue() {
+		return stringValue;
+	}
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+	public int getIntValue() {
+		return intValue;
+	}
+	public void setIntValue(int intValue) {
+		this.intValue = intValue;
+	}
+	
+	
+}
