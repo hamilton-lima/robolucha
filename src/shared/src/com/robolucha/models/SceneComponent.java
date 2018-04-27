9a16e4e8c735661e46e16dc7a492c4a14f914125
Hamilton Lima <hamilton.lima@gmail.com>
2018-04-27 15:10:55 -0400

    Remove life attribute

--- src/shared/src/com/robolucha/models/SceneComponent.java
+++ src/shared/src/com/robolucha/models/SceneComponent.java
@@ -19,103 +19,111 @@ public class SceneComponent extends GameComponent {
 	private boolean colider;
 	private boolean showinRadar;
 	private boolean blockMovement;
-	private double life;
 	private int damage;
 	private String type;
 
-	@Override
-	public String toString() {
-		return "SceneComponent [x=" + x + ", y=" + y + ", width=" + width + ", height=" + height + ", rotation="
-				+ rotation + ", respawn=" + respawn + ", colider=" + colider + ", showinRadar=" + showinRadar
-				+ ", blockMovement=" + blockMovement + ", life=" + life + ", damage=" + damage + ", type=" + type + "]";
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getX() {
-		return x;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public int getY() {
+        return y;
+    }
 
-	public int getY() {
-		return y;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+    public int getWidth() {
+        return width;
+    }
 
-	public int getWidth() {
-		return width;
-	}
+    public void setWidth(int width) {
+        this.width = width;
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public int getHeight() {
+        return height;
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public double getRotation() {
+        return rotation;
+    }
 
-	public double getRotation() {
-		return rotation;
-	}
+    public void setRotation(double rotation) {
+        this.rotation = rotation;
+    }
 
-	public void setRotation(double rotation) {
-		this.rotation = rotation;
-	}
+    public boolean isRespawn() {
+        return respawn;
+    }
 
-	public boolean isRespawn() {
-		return respawn;
-	}
+    public void setRespawn(boolean respawn) {
+        this.respawn = respawn;
+    }
 
-	public void setRespawn(boolean respawn) {
-		this.respawn = respawn;
-	}
+    public boolean isColider() {
+        return colider;
+    }
 
-	public boolean isColider() {
-		return colider;
-	}
+    public void setColider(boolean colider) {
+        this.colider = colider;
+    }
 
-	public void setColider(boolean colider) {
-		this.colider = colider;
-	}
+    public boolean isShowinRadar() {
+        return showinRadar;
+    }
 
-	public boolean isShowinRadar() {
-		return showinRadar;
-	}
+    public void setShowinRadar(boolean showinRadar) {
+        this.showinRadar = showinRadar;
+    }
 
-	public void setShowinRadar(boolean showinRadar) {
-		this.showinRadar = showinRadar;
-	}
+    public boolean isBlockMovement() {
+        return blockMovement;
+    }
 
-	public boolean isBlockMovement() {
-		return blockMovement;
-	}
+    public void setBlockMovement(boolean blockMovement) {
+        this.blockMovement = blockMovement;
+    }
 
-	public void setBlockMovement(boolean blockMovement) {
-		this.blockMovement = blockMovement;
-	}
+    public int getDamage() {
+        return damage;
+    }
 
-	public int getDamage() {
-		return damage;
-	}
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
 
-	public void setDamage(int damage) {
-		this.damage = damage;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
+    @Override
+    public String toString() {
+        return "SceneComponent{" +
+                "x=" + x +
+                ", y=" + y +
+                ", width=" + width +
+                ", height=" + height +
+                ", rotation=" + rotation +
+                ", respawn=" + respawn +
+                ", colider=" + colider +
+                ", showinRadar=" + showinRadar +
+                ", blockMovement=" + blockMovement +
+                ", damage=" + damage +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
