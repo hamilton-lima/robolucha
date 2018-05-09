672e3aaefe423cee440429d49b77299df9bd3f6f
Hamilton Lima <hamilton.lima@gmail.com>
2018-05-09 09:49:59 -0400

    Implements RemoteQueue Service specific for REDIS, should be replaced by KAFKA client

--- src/runner/src/com/robolucha/publisher/RemoteQueue.java
+++ src/runner/src/com/robolucha/publisher/RemoteQueue.java
@@ -1,17 +1,95 @@
 package com.robolucha.publisher;
 
+import com.google.gson.Gson;
+import com.robolucha.runner.Config;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.BehaviorSubject;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPubSub;
 
 public class RemoteQueue {
 
-    public Observable<Boolean> publish(Object subjectToPublish){
-        return Observable.just(Boolean.TRUE);
+    Jedis redis;
+    private Gson gson;
+
+    public RemoteQueue(Config config) {
+        GenericObjectPoolConfig redisConfig = new GenericObjectPoolConfig();
+        JedisPool redisPool = new JedisPool(redisConfig, config.getRedisHost());
+        this.redis = redisPool.getResource();
+
+        this.gson = new Gson();
     }
 
-    //TODO: add super class for the Models
-    public <T>Observable subscribe(Class <T> clazzToSubscribe){
+    public Observable<Long> publish(Object subjectToPublish) {
+        String channel = getChannelName(subjectToPublish);
+        String data = getData(subjectToPublish);
+        this.redis.publish(channel, data);
 
-        // TODO: define the queue name based on the class full name
-        return Observable.just("empty");
+        return Observable.just(this.redis.publish(channel, data));
+    }
+
+    private String getData(Object subjectToPublish) {
+        return this.gson.toJson(subjectToPublish);
+    }
+
+    private String getChannelName(Class clazz) {
+        return clazz.getCanonicalName();
+    }
+
+    private String getChannelName(Object subjectToPublish) {
+        return getChannelName(subjectToPublish.getClass());
+    }
+
+    public <T> BehaviorSubject subscribe(Class<T> clazzToSubscribe) {
+
+        BehaviorSubject<T> result = BehaviorSubject.create();
+        String channel = getChannelName(clazzToSubscribe);
+
+        Thread subscriber = new Thread(new Runnable() {
+            public void run() {
+
+                redis.subscribe(new JedisPubSub() {
+                    @Override
+                    public void onSubscribe(String channel, int subscribedChannels) {
+                        super.onSubscribe(channel, subscribedChannels);
+                    }
+
+                    public void onMessage(String channel, String message) {
+                        T data = gson.fromJson( message, clazzToSubscribe);
+                        result.onNext( data );
+                    }
+
+                }, channel);
+            }
+        });
+
+        result.subscribe(new Observer<T>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {
+
+            }
+
+            @Override
+            public void onNext(T t) {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                subscriber.interrupt();
+            }
+        });
+
+        subscriber.start();
+        return result;
     }
 }
